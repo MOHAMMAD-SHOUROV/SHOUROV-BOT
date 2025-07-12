aac2af2e538f24179a3478b4bf2de89d52a5b3e7
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-12 16:35:02 +0600

    Delete scripts/commands directory

--- scripts/commands/pings.js
+++
@@ -1,9 +0,0 @@
-module.exports.config = {
-  name: "pings",
-  description: "Check if the bot is online",
-  usage: "ping"
-};
-
-module.exports.run = async ({ api, event, args }) => {
-  api.sendMessage("🏓 Pong!", event.threadID, event.messageID);
-};
