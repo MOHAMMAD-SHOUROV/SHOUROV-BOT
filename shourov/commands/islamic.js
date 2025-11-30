dbf6528669ef0f50fe72ce0c3cf02b7aa4fecf2f
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-11-30 08:07:31 -0800

    Update and rename islamic.js to islam.js

--- shourov/commands/islamic.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-module.exports = {
-    config: { name: 'islamic', aliases: ['quran-verse'], role: 0, description: 'Islamic information' },
-    run: async ({ api, event }) => {
-        api.sendMessage('🕌 Islamic Information\n\n📖 Quran & Hadith\n\n✨ Peace be upon you', event.threadID);
-    }
-};
