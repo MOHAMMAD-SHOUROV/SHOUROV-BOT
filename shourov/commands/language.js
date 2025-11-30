a4dc4535da53582dd2c0fa97a281d4cf34f0f159
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-11-30 13:06:32 -0800

    Update language.js

--- shourov/commands/language.js
+++ shourov/commands/language.js
@@ -1,6 +1,40 @@
-module.exports = {
-    config: { name: 'language', aliases: ['lang'], role: 0, description: 'Language settings' },
-    run: async ({ api, event }) => {
-        api.sendMessage('🌍 Language\n\n✅ English\n🇧🇩 Bengali\n🇮🇳 Hindi\n\n💡 Currently: English', event.threadID);
-    }
+module.exports.config = {
+	name: "language",
+	version: "1.0.0",
+	permission: 2,
+	prefix: true,
+	credits: "Nayan",
+	description: "change the bot language",
+	category: "admin",
+	usages: "[vi] [en]",
+	cooldowns: 5
 };
+
+module.exports.run = async ({ api, event, args, getText }) => {
+  let operator = global.config.OPERATOR;
+            if (!operator.includes(event.senderID)) return api.sendMessage(`only bot operators can use this command.`, event.threadID, event.messageID);
+    const { threadID, messageID } = event;
+
+    switch (args[0]) {
+        case "vietnames":
+        case "vi":
+            {
+                return api.sendMessage(`ngôn ngữ đã được chuyển sang tiếng việt`, threadID, () => global.config.language = "vi"); 
+            }
+            break;
+        
+        case "english":
+        case "en":
+            {
+                return api.sendMessage(`language has been converted to english`, threadID, () => global.config.language = "en"); 
+            }
+            break;
+    
+        default:
+            {
+                return api.sendMessage("syntax error, use : language vi/en", threadID, messageID);
+            }   
+            break; 
+            
+    }	
+}
