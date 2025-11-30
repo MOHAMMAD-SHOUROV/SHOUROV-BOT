115e688d07a8cb54d87a488d9c99159d2579db42
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-11-30 05:49:33 -0800

    Update couple.js

--- shourov/commands/couple.js
+++ shourov/commands/couple.js
@@ -1,14 +1,81 @@
-module.exports = {
-    config: { name: 'couple', aliases: ['couple-pic', 'ship'], role: 0, description: 'Create couple profile picture' },
-    run: async ({ api, event }) => {
-        const mentions = Object.keys(event.mentions);
-        if (mentions.length < 2) return api.sendMessage('❌ Mention 2 people for a couple pic!\nUsage: /couple @person1 @person2', event.threadID);
-        
-        const person1 = event.mentions[mentions[0]].replace('@', '');
-        const person2 = event.mentions[mentions[1]].replace('@', '');
-        const pp1 = `https://graph.facebook.com/${mentions[0]}/picture?width=500&height=500`;
-        const pp2 = `https://graph.facebook.com/${mentions[1]}/picture?width=500&height=500`;
-        
-        api.sendMessage(`👫 Couple Pic\n\n${person1} 💕 ${person2}\n\nPP1: ${pp1}\nPP2: ${pp2}`, event.threadID);
+module.exports.config = {
+  name: "couple", 
+  version: "1.0.0", 
+  permission: 2,
+  credits: "Md Shourov Islam",
+  description: "",
+  prefix: true,
+  category: "Love", 
+  usages: "user", 
+  cooldowns: 5,
+  dependencies: {
+        "axios": "",
+        "fs-extra": "",
+        "path": "",
+        "jimp": ""
     }
 };
+ 
+module.exports.onLoad = async() => {
+    const { resolve } = global.nodemodule["path"];
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { downloadFile } = global.utils;
+    const dirMaterial = __dirname + `/cache/canvas/`;
+    const path = resolve(__dirname, 'cache/canvas', 'seophi.png');
+    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/hmKmmam.jpg", path);
+}
+
+async function makeImage({ one, two }) {
+    const fs = global.nodemodule["fs-extra"];
+    const path = global.nodemodule["path"];
+    const axios = global.nodemodule["axios"]; 
+    const jimp = global.nodemodule["jimp"];
+    const __root = path.resolve(__dirname, "cache", "canvas");
+
+    let batgiam_img = await jimp.read(__root + "/seophi.png");
+    let pathImg = __root + `/batman${one}_${two}.png`;
+    let avatarOne = __root + `/avt_${one}.png`;
+    let avatarTwo = __root + `/avt_${two}.png`;
+    
+    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+    
+    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
+    
+    let circleOne = await jimp.read(await circle(avatarOne));
+    let circleTwo = await jimp.read(await circle(avatarTwo));
+    batgiam_img.resize(1024, 712).composite(circleOne.resize(200, 200), 527, 141).composite(circleTwo.resize(200, 200), 389, 407);
+    
+    let raw = await batgiam_img.getBufferAsync("image/png");
+    
+    fs.writeFileSync(pathImg, raw);
+    fs.unlinkSync(avatarOne);
+    fs.unlinkSync(avatarTwo);
+    
+    return pathImg;
+}
+async function circle(image) {
+    const jimp = require("jimp");
+    image = await jimp.read(image);
+    image.circle();
+    return await image.getBufferAsync("image/png");
+}
+
+module.exports.run = async function ({ event, api, args }) {
+    const fs = global.nodemodule["fs-extra"];
+    const { threadID, messageID, senderID } = event;
+    var mention = Object.keys(event.mentions)[0]
+    let tag = event.mentions[mention].replace("@", "");
+    if (!mention) return api.sendMessage("Vui lòng tag 1 người", threadID, messageID);
+    else {
+        var one = senderID, two = mention;
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "Ship ",
+            mentions: [{
+          tag: tag,
+          id: mention
+        }],
+     attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+    }
+}
