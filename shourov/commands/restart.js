4f7cf80fc56a33265dc06f591e003983abfcbdd0
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-11-30 13:55:19 -0800

    Update restart.js

--- shourov/commands/restart.js
+++ shourov/commands/restart.js
@@ -1,6 +1,19 @@
-module.exports = {
-    config: { name: 'restart', aliases: ['reboot'], role: 2, description: 'Restart bot' },
-    run: async ({ api, event }) => {
-        api.sendMessage('🔄 Restarting...\n\n⏳ Please wait', event.threadID);
-    }
+module.exports.config = {
+	name: "restart",
+	version: "7.0.0",
+	permission: 2,
+	credits: "ryuko",
+	prefix: false,
+	description: "restart bot system",
+	category: "admin",
+	usages: "",
+	cooldowns: 0,
+	dependencies: {
+		"process": ""
+	}
 };
+module.exports.run = async function({ api, event, args, Threads, Users, Currencies, models }) {
+  const process = require("process");
+  const { threadID, messageID } = event;
+  api.sendMessage(`restarting ${global.config.BOTNAME} ai, please be patient.`, threadID, ()=> process.exit(1));
+}
