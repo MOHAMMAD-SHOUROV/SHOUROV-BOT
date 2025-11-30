fd008d5f56fa236b313ef4b01f214c926d889305
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-11-30 13:36:24 -0800

    Update and rename pair1.js to pairv2.js

--- shourov/commands/pair1.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    config: { name: 'pair1', aliases: ['match-v1'], role: 0, description: 'Pair/match v1' },
-    run: async ({ api, event }) => {
-        api.sendMessage('👫 Pair V1\n\n💕 Finding matches...', event.threadID);
-    }
-};
