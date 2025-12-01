76f2f76d395ce17fb1e7f888fef32c2fc0a63035
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-11-30 18:04:37 -0800

    Update and rename yes.js to yt.js

--- shourov/commands/yes.js
+++
@@ -1,1 +0,0 @@
-module.exports = { config: { name: 'yes', aliases: ['yep', 'yeah'], role: 0, description: 'Affirmation' }, run: async ({ api, event }) => { api.sendMessage('✅ YES!\n\n👍 Absolutely!\n\n🎯 Confirmed!', event.threadID); } };
