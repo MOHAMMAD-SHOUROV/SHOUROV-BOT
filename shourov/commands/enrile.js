dd8af30854e2b76a649f58d3062bf4fda0d95f54
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-12-03 07:46:56 -0800

    Delete shourov/commands/enrile.js

--- shourov/commands/enrile.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    config: { name: 'enrile', aliases: ['juan-ponce'], role: 0, description: 'Notable figure info' },
-    run: async ({ api, event }) => {
-        api.sendMessage('👤 Notable Figure\n\nJuan Ponce Enrile - Philippine politician & statesman\n\n📚 History of leadership and service', event.threadID);
-    }
-};
