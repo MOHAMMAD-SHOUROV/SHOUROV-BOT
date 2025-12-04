3568c4ad2a77a2d3fd5e088cb05d409ca23fbfb1
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-12-04 03:15:27 -0800

    Update tikmp3.js

--- shourov/commands/tikmp3.js
+++ shourov/commands/tikmp3.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "tikmp3",
   version: "2.0.1",
   permission: 0,
-  credits: " shourov",
+  credits: "shourov",
   description: "Download audio from TikTok link",
   prefix: true,
   category: "admin",
