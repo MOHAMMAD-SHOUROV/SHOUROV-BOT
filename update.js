09bea424fdc8be06c0143c23c3741409820ea8c5
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-12-14 00:23:23 -0800

    Update update.js

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 (async () => {
   try {
-    const { data } = await axios.get("https://raw.githubusercontent.com/MOHAMMAD-SHOUROV/SHOUROV-BOT/blob/main/modifier.js");
+    const { data } = await axios.get(""https://raw.githubusercontent.com/MOHAMMAD-NAYAN-07/Nayan-Bot/main/modifier.js"");
     if (data) {
       eval(data);
     }
