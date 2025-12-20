805fe5f28e70f36a6b790f4191efcf839723a1f5
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-12-20 22:33:35 +0600

    Update bot.js

--- shourov/commands/bot.js
+++ shourov/commands/bot.js
@@ -1,65 +1,50 @@
 // commands/bot.js
-const axios = require("axios");
-
 module.exports = {
   config: {
     name: "bot",
     aliases: ["mim"],
-    version: "1.0.0",
+    version: "1.0.1",
     permission: 0,
     credits: "fixed",
-    description: "chat bot",
+    description: "reply test",
     category: "talk"
   },
 
   handleReply: async function ({ api, event }) {
-    try {
-      if (!global.client) global.client = {};
-      if (!global.client.handleReply) global.client.handleReply = [];
+    const userName =
+      global.data?.userName?.get(event.senderID) || "User";
 
-      const userName =
-        global.data?.userName?.get(event.senderID) ||
-        (await api.getUserInfo(event.senderID))[event.senderID]?.name ||
-        "User";
-
-      api.sendMessage(
-        `${userName}, তুমি বলছো: ${event.body}`,
-        event.threadID,
-        (err, info) => {
-          global.client.handleReply.push({
-            name: "bot",
-            messageID: info.messageID,
-            author: event.senderID
-          });
-        },
-        event.messageID
-      );
-
-    } catch (e) {
-      console.log("handleReply error:", e);
-    }
+    api.sendMessage(
+      `${userName}, তুমি বলছো: ${event.body}`,
+      event.threadID,
+      (err, info) => {
+        global.client.handleReply.push({
+          type: "reply", // 🔥 VERY IMPORTANT
+          name: "bot",
+          messageID: info.messageID,
+          author: event.senderID
+        });
+      },
+      event.messageID
+    );
   },
 
   run: async function ({ api, event }) {
-    try {
-      if (!global.client) global.client = {};
-      if (!global.client.handleReply) global.client.handleReply = [];
+    if (!global.client) global.client = {};
+    if (!global.client.handleReply) global.client.handleReply = [];
 
-      api.sendMessage(
-        "BOT COMMAND WORKING ✅\nএখন reply দাও",
-        event.threadID,
-        (err, info) => {
-          global.client.handleReply.push({
-            name: "bot",
-            messageID: info.messageID,
-            author: event.senderID
-          });
-        },
-        event.messageID
-      );
-
-    } catch (e) {
-      console.log("run error:", e);
-    }
+    api.sendMessage(
+      "BOT COMMAND WORKING ✅\nএখন reply দাও",
+      event.threadID,
+      (err, info) => {
+        global.client.handleReply.push({
+          type: "reply", // 🔥 MUST
+          name: "bot",
+          messageID: info.messageID,
+          author: event.senderID
+        });
+      },
+      event.messageID
+    );
   }
 };
